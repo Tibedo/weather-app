48ced03363830136ac86321af63e2a8e7ce5fcd6
Tibedo <thibaudbaily@hotmail.com>
2022-05-18 12:25:06 +0200

    add citys background Images

--- script2.js
+++ script2.js
@@ -34,7 +34,7 @@ wind.id = "wind"
 wind.className = "box"
 
 const dateOfDay = document.createElement("div")
-dateOfDay.id ="nowDate"
+dateOfDay.id = "nowDate"
 
 dataMeteo.appendChild(city)
 dataMeteo.appendChild(temp)
@@ -51,56 +51,59 @@ let apiCall = function (cityz) {
 
     let url = `https://api.weatherapi.com/v1/forecast.json?key=${APIKEY}&q=${cityz}&days=5&units=metric&lang=fr`;
 
-fetch(url)
-    .then(res => res.json())
-    .then(data => {
-        const meteoList = data.res;
-        console.log(data);
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            const meteoList = data.res;
+            console.log(data);
 
-// ***** CITY ***** //       
-        city.innerHTML = data.location.name;
+            // ***** CITY ***** //       
+            city.innerHTML = data.location.name;
 
-// ***** TEMP ***** //        
-        document.querySelector("#temp").innerHTML = 
-        "<img src='thermometre.png'>" + Math.trunc(data.current.temp_c) + "º";
+            // ***** TEMP ***** //        
+            document.querySelector("#temp").innerHTML =
+                "<img src='thermometre.png'>" + Math.trunc(data.current.temp_c) + "º";
 
-// ***** SUN CLOUDS RAIN ***** //
-       let sunny = document.querySelector("#clouds")
-       if (data.current.condition.text === "Partiellement nuageux") {
-       sunny.innerHTML = "<img src='./day/116.png'>" + data.current.condition.text ;
-        }
-       else if (data.current.condition.text === "Ensoleillé") {
-       sunny.innerHTML = "<img src='./day/113.png'>" + data.current.condition.text ;
-        }
-       else if (data.current.condition.text === "Pluie légère") {
-       sunny.innerHTML = "<img src='./day/296.png'>" + data.current.condition.text ;
-        }
-       else if (data.current.condition.text === "Couvert") {
-       sunny.innerHTML = "<img src='./day/122.png'>" + data.current.condition.text ;
-        }
-       else  {
-       sunny.innerHTML = data.current.condition.text ;
-        }
-// ***** WIND ***** //
-        document.querySelector("#wind").innerHTML = "<img src='wind.png'>" +
-          data.current.wind_kph + 'km/h'; 
+            // ***** SUN CLOUDS RAIN ***** //
+            let sunny = document.querySelector("#clouds")
+            if (data.current.condition.text === "Partiellement nuageux") {
+                sunny.innerHTML = "<img src='./day/116.png'>" + data.current.condition.text;
+            }
+            else if (data.current.condition.text === "Ensoleillé") {
+                sunny.innerHTML = "<img src='./day/113.png'>" + data.current.condition.text;
+            }
+            else if (data.current.condition.text === "Pluie légère") {
+                sunny.innerHTML = "<img src='./day/296.png'>" + data.current.condition.text;
+            }
+            else if (data.current.condition.text === "Couvert") {
+                sunny.innerHTML = "<img src='./day/122.png'>" + data.current.condition.text;
+            }
+            else {
+                sunny.innerHTML = data.current.condition.text;
+            }
+            // ***** WIND ***** //
+            document.querySelector("#wind").innerHTML = "<img src='wind.png'>" +
+                data.current.wind_kph + 'km/h';
 
-// ***** DATE ***** //      
-        document.querySelector("#nowDate").innerHTML = data.current.last_updated;        
-    })
+            // ***** DATE ***** //      
+            document.querySelector("#nowDate").innerHTML = data.current.last_updated;
+        })
 
 
+    // ******* CITY PICTURES ****** //
+    let imgUrl = `https://api.unsplash.com/search/photos?query=${cityz}&client_id=${UNSKEY}&lang=fr`;
 
-     let  imgUrl = `https://api.unsplash.com/photos/random`; 
-    
     fetch(imgUrl)
-    .then(res => res.json())
-    .then(data => {
-        
-        console.log(data);
-})
+        .then(res => res.json())
+        .then(data => {
 
-    .catch(err => console.log('Erreur : ' + err));
+            console.log(data);
+
+            let body = document.querySelector("#body").style.backgroundImage = "url(" + data.results[Math.floor(Math.random() * 10)].urls.regular + ")"
+
+            console.log(body)
+        })
+        .catch(err => console.log('Erreur : ' + err));
 
 }
 
@@ -120,41 +123,41 @@ function dayThreeCall(cityz) {
 
     let url = `https://api.weatherapi.com/v1/forecast.json?key=${APIKEY}&q=${cityz}&days=5&units=metric&lang=fr`;
 
-fetch(url)
-    .then(res => res.json())
-    .then(data => {
-        //const meteoList = data.res;
-        console.log(data);
-       
-        city.innerHTML = data.location.name;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            //const meteoList = data.res;
+            console.log(data);
 
-        document.querySelector("#temp").innerHTML = 
-        "<img src='thermometre.png'>" + Math.trunc(data.forecast.forecastday[2].day.avgtemp_c) + "º";
+            city.innerHTML = data.location.name;
 
-       let sunny = document.querySelector("#clouds")
-       if (data.forecast.forecastday[2].day.condition.text === "Partiellement nuageux") {
-       sunny.innerHTML = "<img src='./day/116.png'>" + data.forecast.forecastday[2].day.condition.text ;
-        }
-       else if (data.forecast.forecastday[2].day.condition.text === "Ensoleillé") {
-       sunny.innerHTML = "<img src='./day/113.png'>" + data.forecast.forecastday[2].day.condition.text ;
-        }
-       else if (data.forecast.forecastday[2].day.condition.text === "Pluie légère") {
-       sunny.innerHTML = "<img src='./day/296.png'>" + data.forecast.forecastday[2].day.condition.text ;
-        }
-       else if (data.forecast.forecastday[2].day.condition.text === "Couvert") {
-       sunny.innerHTML = "<img src='./day/122.png'>" + data.forecast.forecastday[2].day.condition.text ;
-        }
-       else  {
-       sunny.innerHTML = data.forecast.forecastday[2].condition.text ;
-        }
+            document.querySelector("#temp").innerHTML =
+                "<img src='thermometre.png'>" + Math.trunc(data.forecast.forecastday[2].day.avgtemp_c) + "º";
 
-         document.querySelector("#wind").innerHTML = "<img src='wind.png'>" +
-          data.forecast.forecastday[2].day.maxwind_kph + 'km/h'; 
+            let sunny = document.querySelector("#clouds")
+            if (data.forecast.forecastday[2].day.condition.text === "Partiellement nuageux") {
+                sunny.innerHTML = "<img src='./day/116.png'>" + data.forecast.forecastday[2].day.condition.text;
+            }
+            else if (data.forecast.forecastday[2].day.condition.text === "Ensoleillé") {
+                sunny.innerHTML = "<img src='./day/113.png'>" + data.forecast.forecastday[2].day.condition.text;
+            }
+            else if (data.forecast.forecastday[2].day.condition.text === "Pluie légère") {
+                sunny.innerHTML = "<img src='./day/296.png'>" + data.forecast.forecastday[2].day.condition.text;
+            }
+            else if (data.forecast.forecastday[2].day.condition.text === "Couvert") {
+                sunny.innerHTML = "<img src='./day/122.png'>" + data.forecast.forecastday[2].day.condition.text;
+            }
+            else {
+                sunny.innerHTML = data.forecast.forecastday[2].condition.text;
+            }
 
-         document.querySelector("#nowDate").innerHTML = data.forecast.forecastday[2].date;
-         
-    })
-    .catch(err => console.log('Erreur : ' + err));
+            document.querySelector("#wind").innerHTML = "<img src='wind.png'>" +
+                data.forecast.forecastday[2].day.maxwind_kph + 'km/h';
+
+            document.querySelector("#nowDate").innerHTML = data.forecast.forecastday[2].date;
+
+        })
+        .catch(err => console.log('Erreur : ' + err));
 
 }
 
@@ -166,49 +169,49 @@ function dayTwoCall(cityz) {
 
     let url = `https://api.weatherapi.com/v1/forecast.json?key=${APIKEY}&q=${cityz}&days=5&units=metric&lang=fr`;
 
-fetch(url)
-    .then(res => res.json())
-    .then(data => {
-        //const meteoList = data.res;
-        console.log(data);
-       
-        city.innerHTML = data.location.name;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            //const meteoList = data.res;
+            console.log(data);
 
-        document.querySelector("#temp").innerHTML = 
-        "<img src='thermometre.png'>" + Math.trunc(data.forecast.forecastday[1].day.avgtemp_c) + "º";
+            city.innerHTML = data.location.name;
 
-       let sunny = document.querySelector("#clouds")
-       if (data.forecast.forecastday[1].day.condition.text === "Partiellement nuageux") {
-       sunny.innerHTML = "<img src='./day/116.png'>" + data.forecast.forecastday[1].day.condition.text ;
-        }
-       else if (data.forecast.forecastday[1].day.condition.text === "Ensoleillé") {
-       sunny.innerHTML = "<img src='./day/113.png'>" + data.forecast.forecastday[1].day.condition.text ;
-        }
-       else if (data.forecast.forecastday[1].day.condition.text === "Pluie légère") {
-       sunny.innerHTML = "<img src='./day/296.png'>" + data.forecast.forecastday[1].day.condition.text ;
-        }
-       else if (data.forecast.forecastday[1].day.condition.text === "Couvert") {
-       sunny.innerHTML = "<img src='./day/122.png'>" + data.forecast.forecastday[1].day.condition.text ;
-        }
-       else  {
-       sunny.innerHTML = data.forecast.forecastday[1].condition.text ;
-        }
+            document.querySelector("#temp").innerHTML =
+                "<img src='thermometre.png'>" + Math.trunc(data.forecast.forecastday[1].day.avgtemp_c) + "º";
 
-         document.querySelector("#wind").innerHTML = "<img src='wind.png'>" +
-          data.forecast.forecastday[1].day.maxwind_kph + 'km/h'; 
+            let sunny = document.querySelector("#clouds")
+            if (data.forecast.forecastday[1].day.condition.text === "Partiellement nuageux") {
+                sunny.innerHTML = "<img src='./day/116.png'>" + data.forecast.forecastday[1].day.condition.text;
+            }
+            else if (data.forecast.forecastday[1].day.condition.text === "Ensoleillé") {
+                sunny.innerHTML = "<img src='./day/113.png'>" + data.forecast.forecastday[1].day.condition.text;
+            }
+            else if (data.forecast.forecastday[1].day.condition.text === "Pluie légère") {
+                sunny.innerHTML = "<img src='./day/296.png'>" + data.forecast.forecastday[1].day.condition.text;
+            }
+            else if (data.forecast.forecastday[1].day.condition.text === "Couvert") {
+                sunny.innerHTML = "<img src='./day/122.png'>" + data.forecast.forecastday[1].day.condition.text;
+            }
+            else {
+                sunny.innerHTML = data.forecast.forecastday[1].condition.text;
+            }
 
-         document.querySelector("#nowDate").innerHTML = data.forecast.forecastday[1].date;
+            document.querySelector("#wind").innerHTML = "<img src='wind.png'>" +
+                data.forecast.forecastday[1].day.maxwind_kph + 'km/h';
 
-    // ****** DAY THREE CALL ***** //     
-         nextDay.addEventListener('click', function (e) {
-            e.preventDefault();
-            let citys = document.querySelector('.askCity').value;
-        
-            dayThreeCall(citys)
+            document.querySelector("#nowDate").innerHTML = data.forecast.forecastday[1].date;
 
-    })
-    .catch(err => console.log('Erreur : ' + err));  
- })
+            // ****** DAY THREE CALL ***** //     
+            nextDay.addEventListener('click', function (e) {
+                e.preventDefault();
+                let citys = document.querySelector('.askCity').value;
+
+                dayThreeCall(citys)
+
+            })
+                .catch(err => console.log('Erreur : ' + err));
+        })
 
 }
 
@@ -222,14 +225,14 @@ nextDay.addEventListener('click', function (e) {
     e.preventDefault();
     let citys = document.querySelector('.askCity').value;
 
-    dayTwoCall(citys)   
+    dayTwoCall(citys)
 })
 
 
 
 // ********** COMPARE CITYS ********** //
-let cityOneInput = document.createElement("input") 
-let cityTwoInput = document.createElement("input") 
+let cityOneInput = document.createElement("input")
+let cityTwoInput = document.createElement("input")
 let compareButton = document.createElement("button")
 
 cityOneInput.className = "cityInput"
@@ -248,19 +251,19 @@ dates.appendChild(cityTwoInput)
 dates.appendChild(compareButton)
 
 
- function compared() {
+function compared() {
 
-if (cityOneInput.value > cityTwoInput.value) {
-    alert(cityOneInput.value + " " + "is hotter than" + " " + cityTwoInput.value)
-}
+    if (cityOneInput.value > cityTwoInput.value) {
+        alert(cityOneInput.value + " " + "is hotter than" + " " + cityTwoInput.value)
+    }
 
-else {
-    alert(cityOneInput.value + " " + "is colder than" + " " + cityTwoInput.value)
-}
+    else {
+        alert(cityOneInput.value + " " + "is colder than" + " " + cityTwoInput.value)
+    }
 
 }
 
 compareButton.addEventListener("click", compared)
- 
+
 
 
